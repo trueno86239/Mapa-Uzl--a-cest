827b8a5a5bf736bfcd3392acd95ad7781a504b5a
trueno86239 <113853471+trueno86239@users.noreply.github.com>
2024-05-16 19:13:49 +0200

    Add files via upload

---
+++ mapa.py
@@ -0,0 +1,13 @@
+mapa = { 'A': [('B', 1, 'obousměrná')], 
+        'B': [('A', 1, 'obousměrná'), ('C', 3, 'obousměrná'), ('D', 2, 'jednosměrná')],
+        'C': [('B', 3, 'obousměrná'), ('E', 4, 'obousměrná')],
+        'D': [('H', 3, 'obousměrná'), ('G', 2, 'obousměrná')],
+        'E': [('F', 2, 'obousměrná'), ('C', 4, 'obousměrná')],
+        'F': [('E', 2, 'obousměrná'), ('I', 3, 'obousměrná')],
+        'G': [('D', 2, 'obousměrná')],
+        'H': [('D', 3, 'obousměrná'), ('G', 2, 'jednosměrná'), ('F', 2, 'jednosměrná')],
+        'I': [('F', 3, 'obousměrná')]
+        }
+
+for mesta, cesty in mapa.items():
+        print(f"Město {mesta} má cesty: {cesty}")
